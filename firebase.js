ec5c84f5a837b7cc89e8e301234c1143b59413c9
Leenakyun <130434455+Leenakyun@users.noreply.github.com>
2026-07-07 12:36:18 +0900

    Update Firebase configuration with real credentials
    Replaced placeholder Firebase config with actual values.

--- firebase.js
+++ firebase.js
@@ -7,12 +7,14 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-s
 
 // TODO: Firebase 콘솔 -> 프로젝트 설정 -> 앱 등록 후 받은 본인의 config 객체로 교체하세요.
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyB17jVmIkg5K8eEAHH0PIoqA8TY-aeH5kE",
+    authDomain: "resume-5c99e.firebaseapp.com",
+    databaseURL: "https://resume-5c99e-default-rtdb.firebaseio.com",
+    projectId: "resume-5c99e",
+    storageBucket: "resume-5c99e.firebasestorage.app",
+    messagingSenderId: "647551260750",
+    appId: "1:647551260750:web:e26e30aa3fe7e5e638cb1f",
+    measurementId: "G-XT5DT9QBQ7"
 };
 
 // Firebase 초기화
